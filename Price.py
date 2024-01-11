059ceec4ed331792345d5c4ba9092a1b148d4867
fourkies <fourki2002@gmail.com>
2024-01-11 14:53:25 +0700

    update selesai.

--- Price.py
+++ Price.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import pickle
-import warnings
 import locale
+import warnings
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 # Set level warning ke 'ignore' untuk mengabaikan peringatan ini
 warnings.filterwarnings('ignore', category=UserWarning, module='sklearn')
@@ -16,6 +19,27 @@ try:
     # Load dataset
     data = pd.read_csv('Price Range Phone Dataset.csv')
 
+    # Pisahkan fitur (X) dan target (y)
+    X = data.drop('price_range', axis=1)
+    y = data['price_range']
+
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+    # Membuat model Decision Tree Classifier
+    decision_tree_model = DecisionTreeClassifier()
+    decision_tree_model.fit(X_train, y_train)
+
+    # Melakukan prediksi
+    decision_tree_prediction = decision_tree_model.predict(X_test)
+
+    # Menampilkan confusion matrix dan classification report
+    st.subheader('Confusion Matrix:')
+    st.write(confusion_matrix(y_test, decision_tree_prediction))
+
+    st.subheader('Classification Report:')
+    st.write(classification_report(y_test, decision_tree_prediction))
+    
     # Load Decision Tree model
     decision_tree_model = pickle.load(open('DecisionTreeClassifier.pkl', 'rb'))
 except Exception as e:
@@ -64,7 +88,7 @@ if st.checkbox("Detail Kumpulan Dataset Fitur Telepon"):
 
     st.bar_chart(data['price_range'].value_counts())
 
-     # Line chart untuk menunjukkan tren harga terhadap ram
+    # Line chart untuk menunjukkan tren harga terhadap ram
     st.subheader('Tren Harga vs RAM:')
     fig_line = px.line(data, x='ram', y='price_range', title='Tren Harga vs RAM')
     st.plotly_chart(fig_line)
@@ -73,4 +97,3 @@ if st.checkbox("Detail Kumpulan Dataset Fitur Telepon"):
     st.subheader('Persebaran Harga berdasarkan Px_Height dan Px_Width:')
     fig_area = px.area(data, x='px_height', y='px_width', color='price_range', title='Persebaran Harga berdasarkan Px_Height dan Px_Width')
     st.plotly_chart(fig_area)
-
